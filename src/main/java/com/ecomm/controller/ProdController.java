4717b74f16cc79f3c5da99f8c65c2da8a365e2af
sainandn <142480398+sainandn@users.noreply.github.com>
2024-08-28 17:52:35 +0530

    second

--- src/main/java/com/ecomm/controller/ProdController.java
+++ src/main/java/com/ecomm/controller/ProdController.java
@@ -1,22 +1,34 @@
 package com.ecomm.controller;
 
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.ecomm.dto.Prod;
 import com.ecomm.dto.ProdQuant;
 import com.ecomm.dto.Quant;
+import com.ecomm.repo.ProdRepo;
+import com.ecomm.repo.QuantRepo;
 import com.ecomm.service.ProdService;
+
+
+import jakarta.transaction.Transactional;
 
 @RestController
 public class ProdController {
 	
 	@Autowired
 	ProdService prodservice;
+	@Autowired
+	ProdRepo prodrepo;
+	@Autowired
+	QuantRepo Qrepo;
 	
 	@PostMapping("/save")
 	public ResponseEntity<String> saveProdQuant(@RequestBody ProdQuant request ) {
@@ -25,6 +37,18 @@ public class ProdController {
 		prodservice.saveProdQuant(prod, quant);
 		return ResponseEntity.ok("Product details saved successfully!");
 		
+	}
+	@GetMapping("/getproduct")
+	public ResponseEntity<Prod> getProductById(@RequestParam("id") int id) {
+		java.util.Optional<Prod> product = prodrepo.findById(id);
+		return ResponseEntity.ok(product.get());
+	}
+
+	@DeleteMapping("/deleteproduct")
+	public ResponseEntity<String> deleteProductbyId(@RequestParam("id") int id) {
+		Qrepo.deletequantity(id);
+		prodrepo.deleteById(id);
+		return ResponseEntity.ok("Product details deleted successfully!");
 	}
 	
 	
--- src/main/java/com/ecomm/repo/QuantRepo.java
+++ src/main/java/com/ecomm/repo/QuantRepo.java
@@ -1,11 +1,19 @@
 package com.ecomm.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ecomm.dto.Quant;
 
 @Repository
 public interface QuantRepo extends  JpaRepository<Quant, Integer> {
+
+	
+	@Modifying
+	@Query(value = "DELETE FROM Quant where product_id=:productId", nativeQuery = true)
+	void deletequantity(@Param("productId") Integer productId);
 
 }
